93da755acf7670d69bce3803c8f391eb4534c578
jing li <jingli18@bu.edu>
2019-02-11 01:02:10 -0500

    added welcome message

--- patient_moniter.py
+++ patient_moniter.py
@@ -3,42 +3,36 @@ from storage import storage
 from alert import alert_system
 import jasontest
 import json
-import os
+#import os
 
-def main():
+'''
+input module
+from https://github.com/alexlin0625/EC500_Spring19/tree/input_module
+'''
 
-    '''
-    input module
-    from https://github.com/alexlin0625/EC500_Spring19/tree/input_module
-    '''
+patientData = input_module.readSensorData()
+patientInfo = input_module.getPatientInfo()
 
-    patientData = input_module.readSensorData()
-    patientInfo = input_module.getPatientInfo()
-    input_sendor_data = json.loads(patientData)
-    input_patient_data = json.loads(patientInfo)
+'''
+storage_module
+from https://github.com/alexlin0625/EC500_Spring19/tree/storage_module
+'''
 
-    '''
-    storage_module
-    from https://github.com/alexlin0625/EC500_Spring19/tree/storage_module
-    '''
+storage.insert(patientInfo, patientData)
 
-    storage.insert(patientInfo, patientData)
+'''
+alert_system
+from https://github.com/alexlin0625/EC500_Spring19/tree/alert-system
+'''
 
-    '''
-    alert_system
-    from https://github.com/alexlin0625/EC500_Spring19/tree/alert-system
-    '''
-    patient_Data, patient_alert = alert_system.alertCheck(patientInfo, patientData)
-    '''
-    output_module
-    from https://github.com/ec500-software-engineering/exercise-1-modularity-jingli18
-    '''   
-    #patient1.send_alert_to_UI(patient_alert)
+patient_Data, patient_alert = alert_system.alertCheck(patientInfo, patientData)
+'''
+output_module
+from https://github.com/ec500-software-engineering/exercise-1-modularity-jingli18
+'''   
+#patient1.send_alert_to_UI(patient_alert)
 
-    patient_test = jasontest.Patient_output()
-    patient_test.readjson(patient_Data)
-    patient_test.control()
-    print(patient_test.new_patient_dic)
-
-if __name__ == "__main__":
-    main()
+patient_test = jasontest.Patient_output()
+patient_test.readjson(patient_Data)
+patient_test.control()
+print(patient_test.new_patient_dic)
